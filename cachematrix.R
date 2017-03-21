98fa636633adb9bd42f00a60708f1e11ede78045
Ajay Sharma <ajays7671@gmail.com>
2017-03-21 11:03:57 +0530

    R Programming week 3 programming assignment
    This assignment is about caching inverse of a matrix and contains two
    block of functions.In firsrt function, inverse of a matrix is calculated
    and in second function, inverse of a matrix is cached if already done in
    first function and in case inverse is not calculated in first function
    than, inverse is calculated.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The first function makeCacheMatrix is a special "Matrix" #### function
+## It consists list to set and get the value of matrix
+## And also set inverse of matrix and get inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        t <- NULL
+        set <- function(y){
+                x <<- y        
+                t <- NULL
+        }
+        get <- function() x
+        list(set = set, get = get,
 }
 
+## To test the above function we are using a square matrix. ## Just uncomment below 2 lines 
+## and see results
+# x <- matrix(data = c(6,2,8,4), nrow = 2, ncol = 2, byrow = TRUE)
+# makeCacheMatrix(x)
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## The following function creates inverse of the matrix. ##  #However,   it first  checks
+## if inverse is already calculated or not. In case, inverse ## isn't calculated in above function
+## cacheSolve() creates inverse of the matrix
+
+cacheSolve <- function(x, ...){
+        t <- x$getinverse()
+        if(!is.null(x)){
+        stop("getting inversed matrix")
+        return(t)
+        }
+        data <- x$get()
+        t <- solve(data, ...)
+        x$setinverse()
+        t
 }
